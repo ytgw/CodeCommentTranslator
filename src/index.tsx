15e8e1a246f61fcc1be170960d0b3b95aa34a8e4
ytgw <example@example>
2020-12-05 16:16:45 +0900

    大枠の画面デザインを実装

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,121 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
+// ======================================================================
+function Top(): JSX.Element {
+  return (
+    <>
+      <div><Title /><Manual /></div>
+      <div><App /></div>
+    </>
+  );
+}
+
+function Title(): JSX.Element {
+  return (<h1>Source Code Comment Translator</h1>);
+}
+
+function Manual(): JSX.Element {
+  return (<div className="Manual">Manualは未実装</div>);
+}
+
+
+// ======================================================================
+class App extends React.Component {
+  render(): JSX.Element {
+    return (
+      <>
+        <CommentConfig />
+        <AppMain />
+      </>
+    );
+  }
+}
+
+function CommentConfig(): JSX.Element {
+  return (<div className="CommentConfig">CommentConfigは未実装</div>);
+}
+
+
+// ======================================================================
+function AppMain(): JSX.Element {
+  return (
+    <div className="AppMain">
+      <div className="AppMainForm"><SourceInput /></div>
+      <div className="AppMainButton"><PreProcessButton /></div>
+      <div className="AppMainForm"><PreProcessResult /></div>
+      <div className="AppMainButton"><TranslatorButton /></div>
+      <div className="AppMainForm"><TranslationResult /></div>
+    </div>
+  );
+}
+
+
+// ======================================================================
+function SourceInput(): JSX.Element {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="翻訳したいソースコードを入力してください。"
+        onChange={(): void => console.log('SourceInput change')}
+      />
+    </form>
+  );
+}
+
+
+// ======================================================================
+function PreProcessButton(): JSX.Element {
+  return (
+    <button onClick={(): void => console.log('PreProcessButton button pushed')}>
+      前処理
+    </button>
+  );
+}
+
+
+// ======================================================================
+function PreProcessResult(): JSX.Element {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="前処理ボタンを押してください"
+        onChange={(): void => console.log('PreProcessResult change')}
+      />
+    </form>
+  );
+}
+
+
+// ======================================================================
+function TranslatorButton(): JSX.Element {
+  return (
+    <button onClick={(): void => console.log('TranslatorButton button pushed')}>
+      翻訳
+    </button>
+  );
+}
+
+
+// ======================================================================
+function TranslationResult(): JSX.Element {
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="翻訳ボタンを押してください"
+        onChange={(): void => console.log('TranslationResult change')}
+      />
+    </form>
+  );
+}
+
+
+// ======================================================================
 ReactDOM.render(
-  <h1>Code Translator</h1>,
+  <Top />,
   document.getElementById('root')
 );
