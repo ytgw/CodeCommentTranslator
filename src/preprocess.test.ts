ca0376d306543365f5152878d8d5f1a9c3c69ed8
ytgw <example@example>
2020-12-12 20:58:33 +0900

    テストコードを整形

--- src/preprocess.test.ts
+++ src/preprocess.test.ts
@@ -14,39 +14,39 @@ beforeEach(() => {
 // ======================================================================
 test('TypeScript_ラインコメントから始まる行', () => {
   // 末尾の改行の有無の違いを無視するため、変換後のHTMLのスペースをtrimメソッドで削除してから比較
-  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ラインコメントから始まる行.txt', { encoding: 'utf8' }).trimEnd();
-  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ラインコメントから始まる行.txt', { encoding: 'utf8' }).trimEnd();
-  const result = preprocessSourceCode(sampleInput, lang).trimEnd();
+  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ラインコメントから始まる行.txt', {encoding: 'utf8'});
+  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ラインコメントから始まる行.txt', {encoding: 'utf8'});
+  const result = preprocessSourceCode(sampleInput, lang);
 
-  expect(result).toEqual(expectOutput);
+  expect(result.trimEnd()).toEqual(expectOutput.trimEnd());
 });
 
 test('TypeScript_ラインコメントから始まる行が連続', () => {
-  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ラインコメントから始まる行が連続.txt', { encoding: 'utf8' }).trimEnd();
-  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ラインコメントから始まる行が連続.txt', { encoding: 'utf8' }).trimEnd();
-  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput);
+  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ラインコメントから始まる行が連続.txt', {encoding: 'utf8'});
+  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ラインコメントから始まる行が連続.txt', {encoding: 'utf8'});
+  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput.trimEnd());
 });
 
 test('TypeScript_ソースコードの後にラインコメント', () => {
-  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ソースコードの後にラインコメント.txt', { encoding: 'utf8' }).trimEnd();
-  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ソースコードの後にラインコメント.txt', { encoding: 'utf8' }).trimEnd();
-  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput);
+  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ソースコードの後にラインコメント.txt', {encoding: 'utf8'});
+  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ソースコードの後にラインコメント.txt', {encoding: 'utf8'});
+  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput.trimEnd());
 });
 
 test('TypeScript_ラインコメントのみの行が連続し末尾がハイフン', () => {
-  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ラインコメントのみの行が連続し末尾がハイフン.txt', { encoding: 'utf8' }).trimEnd();
-  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ラインコメントのみの行が連続し末尾がハイフン.txt', { encoding: 'utf8' }).trimEnd();
-  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput);
+  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ラインコメントのみの行が連続し末尾がハイフン.txt', {encoding: 'utf8'});
+  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ラインコメントのみの行が連続し末尾がハイフン.txt', {encoding: 'utf8'});
+  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput.trimEnd());
 });
 
 test('TypeScript_ブロックコメントから始まる行', () => {
-  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ブロックコメントから始まる行.txt', { encoding: 'utf8' }).trimEnd();
-  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ブロックコメントから始まる行.txt', { encoding: 'utf8' }).trimEnd();
-  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput);
+  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_ブロックコメントから始まる行.txt', {encoding: 'utf8'});
+  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_ブロックコメントから始まる行.txt', {encoding: 'utf8'});
+  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput.trimEnd());
 });
 
 test('TypeScript_複数行のブロックコメント', () => {
-  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_複数行のブロックコメント.txt', { encoding: 'utf8' }).trimEnd();
-  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_複数行のブロックコメント.txt', { encoding: 'utf8' }).trimEnd();
-  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput);
+  const sampleInput  = fs.readFileSync('./src/testSample/sample_TypeScript_複数行のブロックコメント.txt', {encoding: 'utf8'});
+  const expectOutput = fs.readFileSync('./src/testSample/expect_TypeScript_複数行のブロックコメント.txt', {encoding: 'utf8'});
+  expect(preprocessSourceCode(sampleInput, lang).trimEnd()).toEqual(expectOutput.trimEnd());
 });
