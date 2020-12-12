9c1d720414bc04de05c255827415fd7fa6b5c6de
ytgw <example@example>
2020-12-12 20:27:31 +0900

    ブロックコメント対応の実装

--- src/preprocess.ts
+++ src/preprocess.ts
@@ -5,53 +5,26 @@ type TextProps = {
   type: 'source' | 'commentCommand' | 'comment'
 };
 
+type SearchResult = {
+  propsArray: TextProps[],
+  isEndedInBlockComment: boolean,
+  hasResidual: boolean,
+  residualText: string,
+}
+
 
 // ======================================================================
 class LineProps {
-  private readonly lang: ProgramLang;
   private readonly propsArray: TextProps[];
-  private readonly endInBlockComment: boolean;
+  private readonly isEndedInBlockComment: boolean;
 
   constructor(lang: ProgramLang, raw: string, startInBlockComment: boolean) {
-    this.lang = lang;
-    [this.propsArray, this.endInBlockComment] = this.calcPropsArray(raw, startInBlockComment);
+    const result = (new LinePropsMaker(raw, startInBlockComment, lang)).makeLineProps();
+    this.propsArray = result.propsArray;
+    this.isEndedInBlockComment = result.isEndedInBlockComment;
   }
 
-  calcPropsArray(raw: string, startInBlockComment: boolean): [TextProps[], boolean] {
-    const propsArray: TextProps[] = [];
-
-    // コメント文字列の前後にスペースやタブが0以上入っているパターン
-    // replaceメソッドの目的は正規表現の特殊文字をエスケープ
-    const commentCommands = '\\s*('
-      + this.lang.getLineComment().replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
-      + '|'
-      + this.lang.getBlockComment().start.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
-      + '|'
-      + this.lang.getBlockComment().end.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
-      + ')\\s*';
-    const regex = new RegExp(commentCommands, 'g');
-
-    const array = regex.exec(raw);
-    if (array === null) {
-      const textType = startInBlockComment ? 'comment' : 'source';
-      return [[{raw: raw, type: textType}], startInBlockComment];
-    }
-
-    const commandStartIdx = regex.lastIndex - array[0].length;
-    const commandEndIdx = regex.lastIndex;
-
-    if (commandStartIdx > 0) {
-      propsArray.push({raw: raw.slice(0, commandStartIdx), type: 'source'});
-    }
-    propsArray.push({raw: raw.slice(commandStartIdx, commandEndIdx), type: 'commentCommand'});
-    propsArray.push({raw: raw.slice(commandEndIdx, raw.length), type: 'comment'});
-
-    // TODO endInBlockCommentの算出の実装
-    const endInBlockComment = false;
-    return [propsArray, endInBlockComment];
-  }
-
-  isOnlyComment(): boolean {
+  private isOnlyComment(): boolean {
     return this.propsArray.filter(props => props.type === 'source').length === 0;
   }
 
@@ -59,7 +32,7 @@ class LineProps {
     return this.propsArray.filter(p => p.type === 'comment').map(p => p.raw).join(separator);
   }
 
-  endsHyphen(): boolean {
+  private endsHyphen(): boolean {
     const lastProps = this.propsArray[this.propsArray.length - 1];
     const lastType = lastProps.type === 'comment';
     const lastChar = lastProps.raw[lastProps.raw.length - 1] === '-';
@@ -83,8 +56,121 @@ class LineProps {
     }
   }
 
-  getEndInBlockComment(): boolean {
-    return this.endInBlockComment;
+  getIsEndedInBlockComment(): boolean {
+    return this.isEndedInBlockComment;
+  }
+}
+
+
+// ======================================================================
+class LinePropsMaker {
+  private readonly rawText: string;
+  private readonly startInBlockComment: boolean;
+  private readonly lang: ProgramLang;
+
+  constructor(rawText: string, startInBlockComment: boolean, lang: ProgramLang) {
+    this.rawText = rawText;
+    this.startInBlockComment = startInBlockComment;
+    this.lang = lang;
+  }
+
+  makeLineProps(): {propsArray: TextProps[], isEndedInBlockComment: boolean} {
+    let hasResidual = true;
+    let residualText = this.rawText;
+    let isEndedInBlockComment = this.startInBlockComment;
+
+    const propsArray: TextProps[] = [];
+    while (hasResidual) {
+      let result: SearchResult;
+      if (isEndedInBlockComment) {
+        result = this.searchInBlockComment(residualText);
+      } else {
+        result = this.searchOutBlockComment(residualText);
+      }
+      hasResidual = result.hasResidual;
+      residualText = result.residualText;
+      isEndedInBlockComment = result.isEndedInBlockComment;
+      propsArray.push(...result.propsArray);
+    }
+
+    return {propsArray, isEndedInBlockComment};
+  }
+
+  private searchInBlockComment(text: string): SearchResult {
+    // コメント文字列の前後にスペースやタブが0以上入っているパターン
+    // replaceメソッドの目的は正規表現特殊文字のエスケープ
+    const commentCommands = '\\s*('
+      + this.lang.getBlockComment().end.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
+      + ')\\s*';
+    const regex = new RegExp(commentCommands, 'g');
+    const array = regex.exec(text);
+
+    let hasResidual = false;
+    let residualText = '';
+    let isEndedInBlockComment = true;
+    let propsArray: TextProps[];
+    if (array === null) {
+      propsArray = [{raw: text, type: 'comment'}];
+    } else {
+      propsArray = LinePropsMaker.makePropsArray(array, true, hasResidual);
+      hasResidual = true;
+      residualText = text.slice(regex.lastIndex, text.length);
+      isEndedInBlockComment = false;
+    }
+
+    return {propsArray, residualText, hasResidual, isEndedInBlockComment};
+  }
+
+  private searchOutBlockComment(text: string): SearchResult {
+    // コメント文字列の前後にスペースやタブが0以上入っているパターン
+    // replaceメソッドの目的は正規表現特殊文字のエスケープ
+    const commentCommands = '\\s*('
+      + this.lang.getLineComment().replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
+      + '|'
+      + this.lang.getBlockComment().start.replace(/[\\^$.*+?()[\]{}|]/g, '\\$&')
+      + ')\\s*';
+    const regex = new RegExp(commentCommands, 'g');
+    const array = regex.exec(text);
+
+    let hasResidual = false;
+    let residualText = '';
+    let isEndedInBlockComment = false;
+    let propsArray: TextProps[] = [];
+    if (array === null) {
+      propsArray.push({raw: text, type: 'source'});
+    } else if (array[0].includes(this.lang.getLineComment())) {
+      propsArray = LinePropsMaker.makePropsArray(array, false, hasResidual);
+    } else if (array[0].includes(this.lang.getBlockComment().start)) {
+      hasResidual = true;
+      residualText = text.slice(regex.lastIndex, text.length);
+      isEndedInBlockComment = true;
+      propsArray = LinePropsMaker.makePropsArray(array, false, hasResidual);
+    } else {
+      throw new Error('ここには到達しないはず');
+    }
+
+    return {propsArray, residualText, hasResidual, isEndedInBlockComment};
+  }
+
+  static makePropsArray(array: RegExpExecArray, inBlcokComment: boolean, hasResidual: boolean): TextProps[] {
+    const types: TextProps['type'][] = inBlcokComment
+      ? ['comment', 'commentCommand', 'source']
+      : ['source', 'commentCommand', 'comment'];
+
+    const matchStr = array[0];
+    const inputStr = array.input;
+    const matchStartIndex = array.index;
+    const matchEndIndex = matchStartIndex + matchStr.length;
+
+    const propsArray: TextProps[] = [
+      {raw: inputStr.slice(0, matchStartIndex), type: types[0]},
+      {raw: matchStr, type: types[1]},
+    ];
+    if (!hasResidual) {
+      propsArray.push({raw: inputStr.slice(matchEndIndex, inputStr.length), type: types[2]});
+    }
+
+    return propsArray.filter(p => p.raw !== '');
   }
 }
 
@@ -96,7 +182,7 @@ export function preprocessSourceCode(sourceCode: string, lang: ProgramLang): str
   let startInBlockComment = false;
   for (let i = 0; i < lineList.length; i++) {
     const element: LineProps = new LineProps(lang, lineList[i], startInBlockComment);
-    startInBlockComment = element.getEndInBlockComment();
+    startInBlockComment = element.getIsEndedInBlockComment();
     LinePropsList.push(element);
   }
 
