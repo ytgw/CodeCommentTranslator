5852eb4924ec4e0ee5d6f1980b3e84ea1890362a
ytgw <example@example>
2020-12-14 04:01:53 +0900

    ブロックコメントのテストコードを追加

--- src/sourceCodeAnalyzer.test.ts
+++ src/sourceCodeAnalyzer.test.ts
@@ -4,8 +4,11 @@ import {TypedText, Comment, SourceCodeAnalyzer} from './sourceCodeAnalyzer';
 let analyzer: SourceCodeAnalyzer;
 beforeEach(() => {
   // setup
-  const comment = new Comment('//', '\n');
-  analyzer = new SourceCodeAnalyzer([comment]);
+  const comments: Comment[] = [
+    new Comment('//', '\n'),
+    new Comment('/*', '*/'),
+  ];
+  analyzer = new SourceCodeAnalyzer(comments);
 });
 
 
@@ -26,12 +29,34 @@ test('コメント1行の場合', () => {
   expect(analyzer.type(sourceCode)).toEqual(expectOutput);
 });
 
+test('ブロックコメント1行の場合', () => {
+  const sourceCode = '/* const a = 1 + 1; */';
+  const expectOutput: TypedText[] = [
+    {text: '/* ', type: 'typeChanger'},
+    {text: 'const a = 1 + 1;', type: 'comment'},
+    {text: ' */', type: 'typeChanger'},
+  ];
+  expect(analyzer.type(sourceCode)).toEqual(expectOutput);
+});
+
 test('ソースコードとコメントで全1行の場合', () => {
   const sourceCode = 'const a = 1 + 1;  // result: 2';
   const expectOutput: TypedText[] = [
     {text: 'const a = 1 + 1;', type: 'source'},
     {text: '  // ', type: 'typeChanger'},
     {text: 'result: 2', type: 'comment'},
+  ];
+  expect(analyzer.type(sourceCode)).toEqual(expectOutput);
+});
+
+test('ソースコードとブロックコメントで全1行の場合', () => {
+  const sourceCode = 'const a = 1 /* + 1 */ + 1;';
+  const expectOutput: TypedText[] = [
+    {text: 'const a = 1', type: 'source'},
+    {text: ' /* ', type: 'typeChanger'},
+    {text: '+ 1', type: 'comment'},
+    {text: ' */ ', type: 'typeChanger'},
+    {text: '+ 1;', type: 'source'},
   ];
   expect(analyzer.type(sourceCode)).toEqual(expectOutput);
 });
