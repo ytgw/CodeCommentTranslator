111afcd9f0332a5fbb359c9f605bc1ff6c495ffb
ytgw <example@example>
2020-12-17 01:19:21 +0900

    TitleとManualをjavascriptからhtmlへ移動

--- src/index.tsx
+++ src/index.tsx
@@ -3,32 +3,9 @@ import ReactDOM from 'react-dom';
 import {App} from './app';
 import './index.css';
 
-// ======================================================================
-function Top(): JSX.Element {
-  return (
-    <>
-      <div>
-        <Title />
-        <Manual />
-      </div>
-      <div>
-        <App />
-      </div>
-    </>
-  );
-}
-
-function Title(): JSX.Element {
-  return (<h1>Source Code Comment Translator</h1>);
-}
-
-function Manual(): JSX.Element {
-  return (<div className="Manual">Manualは未実装</div>);
-}
-
 
 // ======================================================================
 ReactDOM.render(
-  <Top />,
+  <App />,
   document.getElementById('root')
 );
