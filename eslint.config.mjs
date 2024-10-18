6a26d71b588d340486dc09faaef50dd5a3a8b6f5
ytgw <tagawa0920@gmail.com>
2024-10-19 00:11:05 +0900

    eslint関連の更新

---
+++ eslint.config.mjs
@@ -0,0 +1,48 @@
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+), {
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.node,
+        },
+
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "commonjs",
+
+        parserOptions: {
+            project: "./tsconfig.json",
+        },
+    },
+
+    rules: {
+        indent: ["warn", 2],
+        quotes: ["warn", "single"],
+        semi: ["warn", "always"],
+        "@typescript-eslint/explicit-function-return-type": "warn",
+    },
+}];
